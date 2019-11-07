36f3946d4ae32a47cc5ae75fc897d4f7a2a9a0fe
宁金 <645619776@qq.com>
2019-11-08 02:55:04 +0800

    threadctl

--- Chapter11/11-4.c
+++ Chapter11/11-4.c
@@ -24,10 +24,10 @@ thr_fn1(void *arg)
 	if ((fp = malloc(sizeof(struct foo))) == NULL)
 		err_sys("can't allocate memory");
 
-	fp->a = 1;
-	fp->b = 2;
-	fp->c = 3;
-	fp->d = 4;
+	(*fp).a = 1;
+	(*fp).b = 2;
+	(*fp).c = 3;
+	(*fp).d = 4;
 
 	printfoo("thread 1: \n", fp);
 	pthread_exit((void *)fp);
